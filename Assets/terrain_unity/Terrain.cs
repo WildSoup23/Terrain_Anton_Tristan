7dd8cf756fbf762f8293d8089b45635a2317f8a0
JagHatarElgiganten123 <d25antli@student.his.se>
2026-01-22 01:17:34 +0100

    Split Vertix is no more

--- Assets/terrain_unity/Terrain.cs
+++ Assets/terrain_unity/Terrain.cs
@@ -9,28 +9,23 @@ public class Terrain
     {
         // Create mesh
         Mesh mesh = new Mesh();
-        NativeArray<Vector3> vertices = new NativeArray<Vector3>(999999999, Allocator.Temp);
-        NativeArray<int> triangles = new NativeArray<int>((size.x * size.y) *6, Allocator.Temp);
+        NativeArray<Vector3> vertices = new NativeArray<Vector3>((size.x + 1)*(size.y + 1), Allocator.Temp);
+        int[] triangles = new int[(size.x * size.y) *6];
         List<Vector3> verticesList = new List<Vector3>();
         
         
         int count = 0;
-        for (int x = 0; x < size.x; x++)
+        for (int x = 0; x < size.x + 1; x++)
         {
-            for (int y = 0; y < size.y; y++)
+            for (int y = 0; y < size.y + 1; y++)
             {
                 vertices[count] = new Vector3(x, 0, y);
-                Debug.Log(vertices[count]);
-                count++;
-                vertices[count] = new Vector3(x, 0, y + 1);
-                Debug.Log(vertices[count]);
-                count++;
-                vertices[count] = new Vector3(x + 1, 0, y);
                 Debug.Log(vertices[count]);
                 count++;
             }
         }
         
+        /*
         for (int x = size.x; x >0; x--)
         {
             for (int y = size.y; y > 0; y--)
@@ -46,25 +41,33 @@ public class Terrain
                 count++;
             }
         }
+        */
         
         count = 0;
+        int check = size.y + 1; 
         for (int x = 0; x < (size.x)*(size.y)*6; x++)
         {
+            if (x + 1 == size.y * check)
+            {
+                break;
+            }
+            
+            else if ((x + 1) % check == 0)
+            {
+                continue;
+            }
+            
             triangles[count] = x;
-            count++;
+            triangles[count + 1] = x + 1;
+            triangles[count + 2] = x + size.y + 1;
+            count += 3;
         }
+        
+        
         
         // Populate the mesh with data
         mesh.SetVertices(vertices);
-        mesh.SetIndices(triangles, MeshTopology.Triangles, 0);
-        
-        // Empty native arrays
-        vertices.Dispose();
-        triangles.Dispose();
-        
-        // Mesh state for render
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+        mesh.SetTriangles(triangles, 0);
         
         // Return 
         return mesh;
