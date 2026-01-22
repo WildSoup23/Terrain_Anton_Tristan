d1c6df16fceef44e5fb4d7e39712023f0d0aec5f
WildSoup23 <tristan06blankenfjell@gmail.com>
2026-01-22 11:55:30 +0100

    Flip

--- Assets/terrain_unity/Terrain.cs
+++ Assets/terrain_unity/Terrain.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public class Terrain
 {
-    public Mesh Regenerate(Vector2Int size, Vector2Int resolution)
+    public Mesh Regenerate(Vector2Int size, Vector2Int resolution, bool flip)
     {
         // Create mesh
         Mesh mesh = new Mesh();
+        NativeArray<Vector3> Vertices = CalculateVertices(size, resolution);
+        int[] Triangles = CalculateTriangles(size, resolution,flip);
+        
+        // Populate the mesh with data
+        mesh.SetVertices(Vertices);
+        mesh.SetTriangles(Triangles, 0);
+        
+        // Return 
+        return mesh;
+    }
+
+    private NativeArray<Vector3> CalculateVertices(Vector2Int size, Vector2Int resolution)
+    {
         NativeArray<Vector3> vertices = new NativeArray<Vector3>((resolution.x + 1)*(resolution.y + 1), Allocator.Temp);
-        int[] triangles = new int[(resolution.x * resolution.y) *6];
         
         int count = 0;
         for (int x = 0; x < resolution.x + 1; x++)
@@ -21,10 +33,16 @@ public class Terrain
                 count++;
             }
         }
-      
+        return vertices;
+    }
+    private int[] CalculateTriangles(Vector2Int size, Vector2Int resolution, bool flip)
+    {
+        int[] triangles = new int[(resolution.x * resolution.y) *6];
         // Forward
-        count = 0;
+        int count = 0;
         int check = resolution.y + 1; 
+        int flipNR = flip ? 2 : 1;
+        
         for (int x = 0; x < (resolution.x+1)*(resolution.y+1); x++)
         {
             if (x + 1 == resolution.x * check)
@@ -36,14 +54,11 @@ public class Terrain
             {
                 continue;
             }
-            
-            Debug.Log(x);
             triangles[count] = x;
             triangles[count + 1] = x + 1;
-            triangles[count + 2] = x + resolution.y + 1;
+            triangles[count + 2] = x + resolution.y + flipNR;
             count += 3;
         }
-        
         // Backward
         for (int x = (resolution.x+1)*(resolution.y+1) - 1; x > 0; x--)
         {
@@ -57,18 +72,11 @@ public class Terrain
             }
             triangles[count] = x;
             triangles[count + 1] = x - 1;
-            triangles[count + 2] = x - resolution.y - 1;
+            triangles[count + 2] = x - resolution.y - flipNR;
             count += 3;
         }
-
         
-        // Populate the mesh with data
-        mesh.SetVertices(vertices);
-        mesh.SetTriangles(triangles, 0);
-        
-        // Return 
-        return mesh;
+        return triangles; 
     }
-    
     
 }
--- Assets/terrain_unity/TerrainScript.cs
+++ Assets/terrain_unity/TerrainScript.cs
@@ -9,8 +9,18 @@ public class TerrainScript : MonoBehaviour
     private Terrain terrain;
     
     //Varibles 
+    [Header("Variables")]
+    [Tooltip("Sets the size of the terrain")]
     public Vector2Int size;
+    [Tooltip("Sets the amount of triangles in the terrain")]
     public Vector2Int resolution;
+    [Tooltip("Sets how the terrains squares are cut into triangles")]
+    public bool flip;
+    
+    //Refrences
+    [Header("Refrences")]
+    [Tooltip("The terrains hight map")]
+    public Texture2D heightmap;
     
     
     
@@ -18,7 +28,7 @@ public class TerrainScript : MonoBehaviour
     {
         if (terrain == null) terrain = new Terrain();
 
-        Mesh mesh = terrain.Regenerate(size, resolution);
+        Mesh mesh = terrain.Regenerate(size, resolution, flip);
         mesh.name = "TerrainMesh";
         GetComponent<MeshFilter>().mesh = mesh;
     }
